631a58c80338faafb0635275f8c7a35447c9b210
Joédyson Bezerra <joedysonbezerra@gmail.com>
2018-01-17 22:48:36 -0200

    menu responsivo implementado

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,26 @@
+var menuAbrir = document.querySelector('.abrir');
 
+menuAbrir.addEventListener('click', function () {
+    document.documentElement.classList.add('menu-ativo');
+});
+
+var menuFechar = document.querySelector('.fechar');
+
+menuFechar.addEventListener('click', function () {
+    document.documentElement.classList.remove('menu-ativo');
+});
+
+var liFechar = document.querySelectorAll('li');
+liFechar.forEach(function (li) {
+    li.addEventListener('click', function () {
+        document.documentElement.classList.remove('menu-ativo');
+    })
+});
+
+
+
+document.documentElement.onclick = function (event) {
+    if (event.target === document.documentElement) {
+        document.documentElement.classList.remove('menu-ativo');
+    }
+};
